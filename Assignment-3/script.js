f5548221780f36cc8689336c82e60bdbb414017c
PrathamHusky07 <kulkarni.prath@northeastern.edu>
2023-02-03 20:05:12 -0500

    Added event listener to script.js

--- Assignment-3/script.js
+++ Assignment-3/script.js
@@ -19,3 +19,31 @@ var t = new Title("CONNECT WITH ME!");
 var table = document.getElementById("myTable");
 var addNew = document.getElementById("add");
 var count = table.rows.length;
+
+function initialize() {
+  for (var i = 1; i < table.rows.length; i += 2) {
+    table.rows[i].innerHTML += "<td></td>";
+  }
+  for (var i = 2; i < table.rows.length; i += 2) {
+    table.rows[i].style.display = "none";
+  }
+  table.rows[0].innerHTML += "<th>Edit</th>";
+}
+
+function addEventListeners() {
+  console.log("addEventListeners", table.rows.length);
+  for (let i = 1; i < table.rows.length; i += 2) {
+    table.rows[i].cells[0].children[0].addEventListener("click", check);
+    table.rows[i].cells[0].children[3].addEventListener("click", () => {
+      showBottom(i);
+    });
+    table.rows[i].cells[8].addEventListener("click", () => {
+      table.rows[i].remove();
+      table.rows[i].remove();
+      check();
+    });
+    table.rows[i].cells[9].addEventListener("click", () => {
+      prompt("Edit Details");
+    });
+  }
+}
